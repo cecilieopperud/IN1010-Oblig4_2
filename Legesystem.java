7f9197ab004c0f47b08877c87da067b93c9864fb
jonasady <62060080+jonasady@users.noreply.github.com>
2020-03-22 15:49:36 +0100

    Update Legesystem.java

--- Legesystem.java
+++ Legesystem.java
@@ -8,12 +8,13 @@ public class Legesystem{
     private static Liste<Legemiddel> legemiddelListe = new Lenkeliste<Legemiddel>();
     private static Liste<Resepter> reseptListe = new Lenkeliste<Resepter>();
     private String fil;
-
-    Legesystem(String f){
+    public Legesystem(String f){
       fil = f;
+      gaaGjennom();
     }
-    public void gaaGjennom() throws FileNotFoundException{
-      File filen = new File("Vedlegg.txt");
+
+    public void gaaGjennom() {
+      File filen = new File(fil);
       Scanner innlesing = null;
 
       try { //try og catch uttalelse for aa sjekke input til innlesing.
@@ -23,7 +24,7 @@ public class Legesystem{
 
       catch (FileNotFoundException e) { //Dersom det blir error blir stringen under skrevet ut.
         System.out.println("Fant desverre ikke filen");
-        throw new FileNotFoundException();
+
       }
 
       int teller = 0;
@@ -36,52 +37,73 @@ public class Legesystem{
         }
 
         if(teller == 1){
-            String [] biter = innlesing.nextLine().split(",");
-            String navn = biter[0];
-            int fnr = Integer.parseInt(biter[1]);
-            Pasient pasient = new Pasient(navn, fnr);
+            String[] biter= innlesing.nextLine().split(",");
+            if(biter.length == 2){
+              String navn = biter[0];
+              String fnr = biter[1];
+              Pasient pasient = new Pasient(navn, fnr);
+         }
+         else{
+          feilmelding();
+
+          }
           }
 
         if(teller == 2){
-          String [] biter = innlesing.nextLine().split(",");
+          String[] biter = innlesing.nextLine().split(",");
+          if (biter.length == 4){
           String navn = biter[0];
           String type = biter[1];
           Double pris = Double.parseDouble(biter[2]);
           Double virkestoff = Double.parseDouble(biter[3]);
-          int styrke = Integer.parseInt(biter[4]);
+
 
           if(type.equals("narkotisk")){
+            int styrke = Integer.parseInt(biter[3]);
             Narkotisk narkotisk = new Narkotisk(navn, pris, virkestoff, styrke);
             legemiddelListe.leggTil(narkotisk);
           }
 
           else if(type.equals("vanedannende")){
+            int styrke = Integer.parseInt(biter[3]);
             Vanedannende vanedannende = new Vanedannende(navn, pris, virkestoff, styrke);
             legemiddelListe.leggTil(vanedannende);
           }
-
-          else if(type.equals("vanlig")){
+        }
+          else if(biter.length == 3 && biter[1].equals("vanlig")){
+            String navn = biter[0];
+            Double pris = Double.parseDouble(biter[2]);
+            Double virkestoff = Double.parseDouble(biter[3]);
             Vanlig vanlig = new Vanlig(navn, pris, virkestoff);
               legemiddelListe.leggTil(vanlig);
           }
+
+        else{
+          feilmelding();
+        }
         }
 
         if(teller == 3){
           String [] biter = innlesing.nextLine().split(",");
+          if(biter.length == 2){
           String navn = biter[0];
           int kontrollid = Integer.parseInt(biter[1]);
-          Pasient pasient = new Pasient(navn, kontrollid);
-
 
           if(kontrollid == 0){
             Lege lege = new Lege(navn);
             legeListe.leggTil(lege);
-          }
+        }
+
           else{
             Spesialist spesialist = new Spesialist(navn, kontrollid);
             legeListe.leggTil(spesialist);
           }
         }
+
+        else{
+          feilmelding();
+        }
+      }
 
         if(teller == 4){
           String [] biter = innlesing.nextLine().split(",");
@@ -92,7 +114,6 @@ public class Legesystem{
           int reit = Integer.parseInt(biter[4]);
 
           Legemiddel legemiddelet = legemiddelListe.hent(legemiddelNummer);
-
           Lege rettLege = null;
           for (int i = 0; i < legeListe.stoerrelse(); i++){
             Lege legen = legeListe.hent(i);
@@ -100,27 +121,63 @@ public class Legesystem{
               rettLege = legen;
             }
           }
+          if(legeNavn!= rettLege.hentNavn()){
+            feilmelding();
+          }
 
           Pasient riktigPasient = null;
           for(int i = 0; i < pasientListe.stoerrelse(); i++){
             if(pasientListe.hent(i).hentID() == pasientID){
               riktigPasient = pasientListe.hent(i);
             }
+            else{
+              feilmelding();
+            }
+          }
+          try{
+
+            if(type.equals("blaa")){
+                rettLege.skrivBlaaResept(legemiddelet, riktigPasient, reit);
+              }
+
+            else if(type.equals("hvit")){
+                rettLege.skrivHvitResept(legemiddelet, riktigPasient, reit);
+              }
+
+            else if(type.equals("militaer")){
+                rettLege.skrivMillitaerResept(legemiddelet, riktigPasient, reit);
+              }
+
+            else if(type.equals("p")){
+              rettLege.skrivPResept(legemiddelet, riktigPasient);
+                }
+
+              }
+
+          catch(UlovligUtskrift u){
+            System.out.println("Ugyldig");
           }
 
-          if(type.equals("blaa")){
-            rettLege.skrivBlaaResept(legemiddelet, rettLege, riktigPasient, reit);
-          }
-          if(type.equals("hvit")){
-            rettLege.skrivHvitResept(legemiddelet, rettLege, riktigPasient, reit);
-          }
-          if(type.equals("militaer")){
-            rettLege.skrivMillitaerResept(legemiddelet, rettLege, riktigPasient, reit);
-          }
-          if(type.equals("p")){
-            rettLege.skrivPResept(legemiddelet, rettLege, riktigPasient);
-          }
         }
       }
+}
+    public Liste<Resepter> returnerReseptListe(){
+      return reseptListe;
+      }
+
+    public Liste<Legemiddel> retunerLegemiddelListe(){
+      return legemiddelListe;
     }
-  }
+
+    public Liste<Lege> retunerLegeListe(){
+      return legeListe;
+    }
+
+    public Liste<Pasient> returnerPasientListe(){
+      return pasientListe;
+    }
+    public void feilmelding(){
+      System.out.println("Ugyldig input");
+    }
+
+}
