e0a379a2217aab17dbfbdf1838ed701f4cff8761
jonasady <62060080+jonasady@users.noreply.github.com>
2020-03-23 15:49:39 +0100

    Add files via upload

--- TestProgram.java
+++ TestProgram.java
@@ -1,20 +1,10 @@
-class TestProgram{
-  public static void main(String[] args){
-    Pasient pasient = new Pasient(240999, "Cecilie");
-    Lege lege = new Lege("Benjamin");
-    Vanlig legemiddel = new Vanlig("Jonas", 125, 5);
-    BlaaResept resept = new BlaaResept(legemiddel, lege, pasient, 3);
-    Liste<Pasient> pasientListe = new Lenkeliste<Pasient>();
-    Liste<Lege> legeListe = new Lenkeliste<Lege>();
-    Liste<Legemiddel> legemiddelListe = new Lenkeliste<Legemiddel>();
-    Liste<Resepter> reseptListe = new Lenkeliste<Resepter>();
-    pasientListe.leggTil(pasient);
-
-    System.out.println(pasient.hentNavn());
-    System.out.println(pasient.hentID());
-    pasient.leggTilResept(resept);
-    Stabel<Resepter> denne = pasient.hentReseptene();
-
-    System.out.println(denne.hentLegemiddel());
+public class TestProgram{
+  public static void main(String[]args){
+    Legesystem l = new Legesystem("Vedlegg.txt");
+    //Liste<Resepter> reseptListe = l.returnerReseptListe();
+    Liste<Legemiddel> legemiddelListe = l.retunerLegemiddelListe();
+    //System.out.println(reseptListe);
+    System.out.println(legemiddelListe);
   }
+
 }
